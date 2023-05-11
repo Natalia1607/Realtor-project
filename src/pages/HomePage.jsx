fb02169e60c0756442123b514ea09ea368fe26e6
Natalia Gilyazova <gilyazovan97@mail.ru>
2023-05-11 19:09:49 +0300

    first commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,24 @@
+import React, { Fragment } from "react";
+import Hero from "../components/Layout/Hero";
+import Purpose from "../components/Layout/Purpose";
+import Properties from "../components/Layout/Properties";
+import OurService from "../components/Layout/OurService";
+import Testimonial from "../components/Layout/Testimonial";
+import FAQs from "../components/Layout/FAQs";
+import Footer from "../components/Layout/Footer";
+
+const HomePage = () => {
+  return (
+    <Fragment>
+      <Hero />
+      <Purpose />
+      <Properties />
+      <OurService />
+      <Testimonial />
+      <FAQs />
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default HomePage;
